7cf0e1117d552dc14b7b44f49238dfc7e4078567
ikozul00 <ivana.kozul5@gmail.com>
2021-04-11 14:58:41 +0200

    Trying to connect with database

--- app/src/main/java/com/example/quiz/Question.java
+++ app/src/main/java/com/example/quiz/Question.java
@@ -1,11 +1,14 @@
 package com.example.quiz;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Question extends Activity {
     private boolean answerTrue;
@@ -26,32 +29,57 @@ public class Question extends Activity {
     }
 
     public void onClickButton(View v){
-        switch (v.getId()){
-            case R.id.radio_button1:
-                RadioButton button1=findViewById(R.id.radio_button1);
-                checkAnswer(button1.getText().toString());
-                break;
-            case R.id.radio_button2:
-                RadioButton button2=findViewById(R.id.radio_button2);
-                checkAnswer(button2.getText().toString());
-                break;
-            case R.id.radio_button3:
-                RadioButton button3=findViewById(R.id.radio_button3);
-                checkAnswer(button3.getText().toString());
-                break;
-            case R.id.radio_button4:
-                RadioButton button4=findViewById(R.id.radio_button4);
-                checkAnswer(button4.getText().toString());
-                break;
-            case R.id.nextButton:
+        int x;
+        RadioGroup group=findViewById(R.id.answersGroup);
+        x=group.getCheckedRadioButtonId();
+        RadioButton button1 = findViewById(R.id.radio_button1);
+        RadioButton button2 = findViewById(R.id.radio_button2);
+        RadioButton button3 = findViewById(R.id.radio_button3);
+        RadioButton button4 = findViewById(R.id.radio_button4);
+        if(v.getId()==R.id.nextButton){
+            if(x==-1){
+                Context context = getApplicationContext();
+                String msg="You need to choose one of the options";
+                int duration= Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, msg, duration);
+                toast.show();
+            }
+            else{
                 nextQuestion();
-                break;
+            }
+        }
+        else {
+            switch (x) {
+                case R.id.radio_button1:
+                    checkAnswer(button1.getText().toString());
+                    button2.setEnabled(false);
+                    button3.setEnabled(false);
+                    button4.setEnabled(false);
+                    break;
+                case R.id.radio_button2:
+                    checkAnswer(button2.getText().toString());
+                    button1.setEnabled(false);
+                    button3.setEnabled(false);
+                    button4.setEnabled(false);
+                    break;
+                case R.id.radio_button3:
+                    checkAnswer(button3.getText().toString());
+                    button2.setEnabled(false);
+                    button1.setEnabled(false);
+                    button4.setEnabled(false);
+                    break;
+                case R.id.radio_button4:
+                    checkAnswer(button4.getText().toString());
+                    button2.setEnabled(false);
+                    button3.setEnabled(false);
+                    button1.setEnabled(false);
+                    break;
+            }
         }
     }
 
     private void checkAnswer(String answer){
         TextView msgView = findViewById(R.id.messageLabel);
-        msgView.setText(messageCorrect);
         if(answer.equals(correctAnswer)){
             msgView.setText(messageCorrect);
         }
@@ -61,9 +89,21 @@ public class Question extends Activity {
     }
 
     private void nextQuestion(){
+        //setting up initial state
+        RadioButton button1 = findViewById(R.id.radio_button1);
+        RadioButton button2 = findViewById(R.id.radio_button2);
+        RadioButton button3 = findViewById(R.id.radio_button3);
+        RadioButton button4 = findViewById(R.id.radio_button4);
+        RadioGroup group=findViewById(R.id.answersGroup);
+        group.clearCheck();
+        button1.setEnabled(true);
+        button2.setEnabled(true);
+        button3.setEnabled(true);
+        button4.setEnabled(true);
+        TextView msgView = findViewById(R.id.messageLabel);
+        msgView.setText("");
         String question="question 2";
         TextView textView = findViewById(R.id.questionText);
         textView.setText(question);
-
     }
 }
